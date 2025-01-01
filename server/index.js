c383e76630ec22fca48e1882bda21e55257edcc6
rishi-bathija <bathijarishi@gmail.com>
2025-01-01 11:57:40 +0530

    url issue fixed

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: ["http://localhost:3000", "https://studynotion-frontend-chi-rosy.vercel.app/"],
+        origin: ["http://localhost:3000", "https://studynotion-frontend-chi-rosy.vercel.app"],
         credentials: true,
     })
 );
